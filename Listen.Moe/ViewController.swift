18ca389f7669ab5ea44a6c15847e9c9597e14af1
Kel <son2kel@gmail.com>
2017-10-06 21:23:14 -0600

    Initial Commit

---
+++ Listen.Moe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Listen.Moe
+//
+//  Created by Kelson Vella on 10/6/17.
+//  Copyright © 2017 Disre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
